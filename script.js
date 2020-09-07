221b805429d4ed6d175e20b8b4f2f1cda383254f
Dmytro <dmytro.kurilov@freshcodeit.net>
2020-09-07 14:40:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert ('Hello');
+
+// const result = confirm("Are you here?");
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your surname?', '');
+// answers[2] = prompt('how old are you?', '');
+
+console.log(typeof(answers));
