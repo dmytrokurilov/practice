20a754a6338408d051729c9541cdcd0a399fd820
Dmytro <dmytro.kurilov@freshcodeit.net>
2020-09-08 11:43:29 +0300

    commit urok 18

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const numberOfFilms = +prompt('How many movies did you watch?', '1-3');
+'use strict';
+
+    let numberOfFilms;
+
+    function start() {
+        numberOfFilms = +prompt('How many movies did you watch?', '1-3');   
+
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+               numberOfFilms = +prompt('How many movies did you watch?', '1-3');    
+        }
+}
+
+start();
 
 const personalMovieDB = {
 
@@ -7,14 +19,55 @@ const personalMovieDB = {
         actors: {},
         genres: [],
         privat: false
+};
+
+    function showMyDB(hidden) {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    }
+
+    function writeYourGenres() {
+        for (let i = 1; i <= 3; i++) {
+                personalMovieDB.genres[i -1] = prompt(`Your favourite genre pod nomerom ${i}`, '');
+        }
+    }
+writeYourGenres();
+
+    function rememberMyFilms() {
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One of last seen movies?', ''),
+          b = prompt('Rate this movie?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log ('error');
+        i--;
+    }
+    
+}
+    }
+
+rememberMyFilms();
+
+
+    function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+            console.log ("watched dovolno malo filmov");
+    }   else if (personalMovieDB.count >= 10 &&  personalMovieDB.count < 30) {
+           console.log ("classic zritel");
+    }
+        else if (personalMovieDB.count >= 30) {
+            console.log ("you are kinoman");
+        }
+        else {
+            console.log ("error");
+        }
+   
 }
 
-const a = prompt('One of last seen movies?', ''),
-      b = prompt('Rate this movie?', ''),
-      c = prompt('One of last seen movies?', ''),
-      d = prompt('Rate this movie?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+rememberMyFilms();
 
 console.log(personalMovieDB);
